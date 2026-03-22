07df0485c9ffa8dd042a89c5283d96b2c0a4022b
Ruser <user@DESKTOP-NS5BTI0>
2026-03-23 00:21:03 +0100

    debut de projet avec les onglet et tout les entitées créer les enums/modèles mais le projet ne compile pas encore (Lombok non résolu).

---
+++ src/test/java/padelSite/padelServivce/PadelServivceApplicationTests.java
@@ -0,0 +1,13 @@
+package padelSite.padelServivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelServivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
